ca9eee56c75fb8339ec86529ea3cd76fc77e1947
Brian Schubert <brianm.schubert@gmail.com>
2018-09-13 21:31:41 -0400

    Infer buffer lengths in c test cases

--- ctest/test.c
+++ ctest/test.c
@@ -34,7 +34,7 @@ static bool test_caesar(void) {
     const purecipher_obj_t caesar = purecipher_cipher_caesar();
     const uint8_t *expected = (uint8_t *) "Zh dwwdfn dw gdzq.";
 
-    uint8_t buffer[19] = "We attack at dawn.";
+    uint8_t buffer[] = "We attack at dawn.";
 
     purecipher_encipher_buffer(caesar, buffer, sizeof(buffer));
 
@@ -49,7 +49,7 @@ static bool test_rot13(void) {
     const purecipher_obj_t rot13 = purecipher_cipher_rot13();
     const uint8_t *expected = (uint8_t *) "Ybiryl cyhzntr, gur Abejrtvna Oyhr.";
 
-    uint8_t buffer[36] = "Lovely plumage, the Norwegian Blue.";
+    uint8_t buffer[] = "Lovely plumage, the Norwegian Blue.";
 
     purecipher_encipher_buffer(rot13, buffer, sizeof(buffer));
 
@@ -64,7 +64,7 @@ static bool test_leet(void) {
     const purecipher_obj_t leet = purecipher_cipher_leet();
     const uint8_t *expected = (uint8_t *) "Pur3 c!ph3rs @r3 1h3 BE5Ti";
 
-    uint8_t buffer[27] = "Pure ciphers are the BEST!";
+    uint8_t buffer[] = "Pure ciphers are the BEST!";
 
     purecipher_encipher_buffer(leet, buffer, sizeof(buffer));
 
